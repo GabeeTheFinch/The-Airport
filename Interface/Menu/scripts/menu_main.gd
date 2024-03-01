c5b8a3e43cf113a1442487ae01b6bb8141dbdfb9
TheDarkny974 <thedarkny.gabriel@gmail.com>
2024-03-01 17:27:30 +0400

    Map selector and Transitions are now done through transition_screen.gd

--- Interface/Menu/scripts/menu_main.gd
+++ Interface/Menu/scripts/menu_main.gd
@@ -4,17 +4,12 @@ extends CanvasLayer
 @onready var Audio_Base = $Control/Audio_Base
 @onready var Settings_Menu = $Menu_settings
 @onready var TransitionScreen := $"Transition Screen"
+@onready var MapSelectScreen := $Control/Map_Selector
 
-const Map_test = preload("res://Maps/map_player_testing.tscn")
 
 func _on_btn_start_pressed():
-	Audio_Start.play()
-	TransitionScreen.Fade_in(0.5)
-
-
-
-func _on_transition_screen_transition():
-	get_tree().change_scene_to_file("res://Maps/map_player_testing.tscn")
+	Audio_Base.play()
+	MapSelectScreen.visible = true
 
 func _on_btn_settings_pressed():
 	Audio_Base.play()
@@ -22,3 +17,13 @@ func _on_btn_settings_pressed():
 		Settings_Menu.visible = false
 	else:
 		Settings_Menu.visible = true
+
+
+func _on_test_map_pressed():
+	Audio_Start.play()
+	TransitionScreen.Fade_Transition(0.5, "res://Maps/map_player_testing.tscn")
+	pass # Replace with function body.
+func _on_zone_1_blocky_pressed():
+	Audio_Start.play()
+	TransitionScreen.Fade_Transition(0.5, "res://Maps/Zone1_blocky.tscn")
+	pass # Replace with function body.
